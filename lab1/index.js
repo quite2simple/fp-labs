5ed25544a31d718e1b76f7d61906912f386e8985
quite2simple <simplechuck882@gmail.com>
2025-02-08 18:59:25 +0300

    Lab requirements met

--- lab1/index.js
+++ lab1/index.js
@@ -1,3 +1,17 @@
-import { keepEvenOnly, squaredArray, arraySum, keepHasLengthOnly, applyFuncToElements } from "./functions";
+import { keepEvenOnly, squaredArray, arraySum, keepHasLengthOnly, applyFuncToElements } from "./functions.js";
 
 
+const numberArray = [4, 7, -2, 8, 2, 3, 9, 11, 9, 10];
+const objectArray = [11, 2, 5, "43", 18, true, 6, {a: 5, b: "hello"}, 80, 15, NaN, 12, -4, 10];
+const barrier = 10;
+
+const squaredSum = arraySum(squaredArray(keepEvenOnly(numberArray)));
+
+const filteredObjects = objectArray.filter((n) => typeof n === "number" && n > barrier);
+const mean = arraySum(filteredObjects) / filteredObjects.length;
+
+
+console.log(squaredSum);
+
+console.log(filteredObjects);
+console.log(mean);
