569c9ead40457ea6144a4cc32e6dd62abc5caf32
quite2simple <simplechuck882@gmail.com>
2025-02-10 23:10:18 +0300

    Added semicolons

---
+++ lab2/src/todo.js
@@ -0,0 +1,40 @@
+export const createTodo = () => {
+    const template = document.getElementById('todo');
+    const clone = template.content.cloneNode(true);
+
+    return clone;
+}
+
+export const setupTodo = (todo, onChange, data) => {
+    const todoCompleted = todo.querySelector('.todo-completed');
+    const todoText = todo.querySelector('.todo-text');
+    const todoDelete = todo.querySelector('.todo-delete');
+
+    const todoData = {
+        completed: data.completed,
+        text: data.checked,
+        deleted: false
+    };
+
+    todoCompleted.addEventListener('change', () => {
+        todoData.completed = todoCompleted.checked;
+        onChange();
+    });
+    todoText.addEventListener('input', () => todoData.text = todoText.value);
+    todoDelete.addEventListener('click', () => {
+        todoData.deleted = true;
+        onChange();
+        todo.remove();
+    });
+
+    return todoData;
+}
+
+export const serializeTodos = (todos) => {
+    return JSON.stringify(todos.filter((todo) => !todo.deleted));
+}
+
+export const loadTodos = (storage) => {
+    const todos = JSON.parse(storage.getItem('todos'));
+    return todos;
+}
