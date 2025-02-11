0ab95ff8dab005d50a54d4deb732a0029f56b8c8
quite2simple <simplechuck882@gmail.com>
2025-02-11 09:19:39 +0300

    Counters working, added a custom favicon

--- lab2/src/main.js
+++ lab2/src/main.js
@@ -5,11 +5,21 @@ const app = document.querySelector('#app');
 const todoTemplate = document.querySelector('#todo');
 const todoList = document.querySelector('#todo-list');
 const addTodoButton = document.querySelector('#add-todo');
+const totalCounter = document.querySelector('#total-counter');
+const completedCounter = document.querySelector('#completed-counter');
 
 const savedTodos = loadTodos(localStorage) || [];
 const todoDataList = [];
 
-const saveToLocalStorage = (todos) => localStorage.setItem('todos', serializeTodos(todos));
+const updateCounters = () => {
+  totalCounter.innerHTML = `Total: ${todoDataList.filter((todo) => !todo.deleted).length}`;
+  completedCounter.innerHTML = `Completed: ${todoDataList.filter((todo) => todo.completed && !todo.deleted).length}`;
+}
+
+const saveToLocalStorage = (todos) => {
+  localStorage.setItem('todos', serializeTodos(todos));
+  updateCounters();
+};
 
 const newTodo = (data={}) => {
   const todo = createTodo(todoTemplate);
