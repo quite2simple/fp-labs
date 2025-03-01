aefb7dc2537c2dfa254cfc381c1bf71e7bda517b
quite2simple <simplechuck882@gmail.com>
2025-03-01 13:30:41 +0300

    Last function implemented, fixed another function, added tests and function calls

--- lab3/functions.ts
+++ lab3/functions.ts
@@ -21,15 +21,35 @@ export interface HasCredits {
     credits: number;
 }
 
+export interface CreditedUser extends HasCredits {
+    id: number;
+    username: string;
+}
 
 /**
  * Sorts an array of objects by the 'credits' property in ascending or descending order.
  *
- * @param {Type[]} arr - The array of objects to sort.
+ * @param {T[]} arr - The array of objects to sort.
  * @param {boolean} asc - If true, the array is sorted in ascending order (smallest credits first).
  *                         If false, the array is sorted in descending order (largest credits first).
- * @returns {Type[]} The sorted array.
+ * @returns {T[]} The sorted array.
  */
-export const sortByCredits = <Type extends HasCredits>(arr: Type[], asc: boolean): Type[] => {
+export const sortByCredits = <T extends HasCredits>(arr: T[], asc: boolean): T[] => {
     return arr.sort((a, b) => (asc ? a.credits - b.credits : b.credits - a.credits));
+}
+
+/**
+ * Wraps a function and logs its calls, including arguments and return value.
+ * 
+ * @template T - The type of the function to be wrapped.
+ * @param {T} fn - The function to be logged.
+ * @returns {T} A new function that logs its calls and then delegates to the original function.
+ */
+export const loggedFunctionCall = <T extends (...args: Parameters<T>) => ReturnType<T>>(fn: T): T => {
+    return ((...args: Parameters<T>): ReturnType<T> => {
+        console.log(`Calling function ${fn.name} with arguments:`, args);
+        const result = fn(...args);
+        console.log(`Function ${fn.name} returned:`, result);
+        return result;
+    }) as T;
 }
--- lab3/functions_test.ts
+++ lab3/functions_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { divisibleBy3Only, dashJoin } from "./functions.ts";
+import { divisibleBy3Only, dashJoin, sortByCredits, CreditedUser } from "./functions.ts";
 
 Deno.test(function divisibleBy3OnlyTest() {
     assertEquals(divisibleBy3Only([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [3, 6, 9]);
@@ -11,4 +11,15 @@ Deno.test(function dashJoinTest() {
     assertEquals(dashJoin(["a", "b", "c"]), "a-b-c");
     assertEquals(dashJoin(["a", "b", "c", "d"]), "a-b-c-d");
     assertEquals(dashJoin(["a", "b", "c", "d", "e"]), "a-b-c-d-e");
+});
+
+Deno.test(function sortByCreditsTest() {
+    const arr = [
+        {id: 549, username: "jh442", credits: 100},
+        {id: 4992, username: "boe-6", tester: true, credits: 45},
+        {id: 4, username: "Arson", credits: 2},
+        {id: 4, username: "Lemming", credits: 11},
+    ];
+    assertEquals(sortByCredits<CreditedUser>(arr, true), arr.sort((a, b) => a.credits - b.credits));
+    assertEquals(sortByCredits<CreditedUser>(arr, false), arr.sort((a, b) => b.credits - a.credits));
 });
